a37d59cf822cbcc12fa27808fb3937ce2c623275
tapatiLarissa <tapatilarissa@gmail.com>
2026-01-15 12:42:45 +0100

    Update DemoGithubApplication.java

--- src/main/java/com/example/demo_github/DemoGithubApplication.java
+++ src/main/java/com/example/demo_github/DemoGithubApplication.java
@@ -1,23 +1,11 @@
-package com.example.demo_github;
+package com.example.demo;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.RestController;
 
 @SpringBootApplication
-public class DemoGithubApplication {
-
-	public static void main(String[] args) {
-		SpringApplication.run(DemoGithubApplication.class, args);
-	}
-
-}
-
-@RestController
-class HelloController {
-    @GetMapping("/")
-    public String hello() {
-        return "Hello, World!";
+public class DemoApplication {
+    public static void main(String[] args) {
+        SpringApplication.run(DemoApplication.class, args);
     }
 }
